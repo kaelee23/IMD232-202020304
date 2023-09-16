7268650bb794467859946ba36dc0911929436db7
kaelee23 <dahnlee@naver.com>
2023-09-17 01:12:43 +0900

    12

--- src/w2/assignment/basic/script/basicDrawing.js
+++ src/w2/assignment/basic/script/basicDrawing.js
@@ -8,13 +8,8 @@ function draw() {
 
   rectMode(CORNER);
   ellipse(100, 100, 50, 50);
-  rect(100, 100, 50, 50);
-  ellipse(200, 100, 50, 25);
-  rect(200, 100, 25, 50);
 
   rectMode(CENTER);
   rect(300, 100, 50, 50);
   ellipse(300, 100, 50, 50);
-  rect(400, 100, 50);
-  ellipse(400, 100, 25, 50);
 }
