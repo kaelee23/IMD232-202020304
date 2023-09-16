7e9e87e7d12807e8afec60245c5bc3880458f738
kaelee23 <dahnlee@naver.com>
2023-09-16 20:12:58 +0900

    12

--- src/w2/sketches/sketch8pre/script/sketch.js
+++ src/w2/sketches/sketch8pre/script/sketch.js
@@ -1,9 +1,9 @@
 function setup() {
   setCanvasContainer('canvas-goes-here', 3, 2, true);
-  background('Gray');
+  background(127);
 }
 
 function draw() {
-  background('Gray');
+  background(255, 0, 0);
   circle(mouseX, mouseY, width * 0.1, height * 0.1);
 }
