af16654fee48e43435074b91a9c1b2b6f2e1b112
kaelee23 <dahnlee@naver.com>
2023-09-16 15:22:50 +0900

    22분

--- src/w2/sketches/sketch1/script/sketch.js
+++ src/w2/sketches/sketch1/script/sketch.js
@@ -1,12 +1,3 @@
-function setup() {
-  setCanvasContainer('canvas', 3, 2, true);
-
-  background(255);
-  noStroke();
-  fill('red');
-}
-
-function draw() {
-  background(255);
-  circle(mouseX, mouseY, 50);
-}
+alert('나의 홈페이지에 당도한 것을 환영하오. 젊은이여');
+let usertype = prompt('자네 이름이..?', '홍갈동');
+confirm('그러니까 자네 이름이 ' + usertype + '라는 겐가');
