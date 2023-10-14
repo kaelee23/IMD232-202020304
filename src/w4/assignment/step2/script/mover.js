eda842d59f1ecfa25f94fcf487a04086ead31a5b
kaelee23 <dahnlee@naver.com>
2023-10-15 02:13:23 +0900

    ㄴ

---
+++ src/w4/assignment/step2/script/mover.js
@@ -0,0 +1,52 @@
+class Mover {
+  constructor(x, y, mass) {
+    this.pos = createVector(x, y);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.mass = mass;
+    this.rad = mass; // Increased the size to 40
+    this.isHover = false;
+  }
+
+  applyForce(force) {
+    const f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  edgeBounce() {
+    const bounce = -0.7;
+    if (this.pos.x < this.rad) {
+      this.pos.x = this.rad;
+      this.vel.x *= bounce;
+    } else if (this.pos.x > width - 1 - this.rad) {
+      this.pos.x = width - 1 - this.rad;
+      this.vel.x *= bounce;
+    }
+    if (this.pos.y > height - 1 - this.rad) {
+      this.pos.y = height - 1 - this.rad;
+      this.vel.y *= bounce;
+    } else if (this.pos.y < this.rad) {
+      // Gradually return to the initial position if out of bounds at the top
+      const returnForce = createVector(
+        initialPos.x - this.pos.x,
+        initialPos.y - this.pos.y
+      ).mult(0.05);
+      this.applyForce(returnForce);
+    }
+  }
+
+  display() {
+    this.isHover =
+      (this.pos.x - mouseX) ** 2 + (this.pos.y - mouseY) ** 2 <= this.rad ** 2;
+
+    noStroke();
+    fill(this.isHover ? color(255, 0, 0) : color(0));
+    ellipse(this.pos.x, this.pos.y, 2 * this.rad);
+  }
+}
--- src/w4/assignment/step2/script/sketch.js
+++ src/w4/assignment/step2/script/sketch.js
@@ -1,12 +1,56 @@
-function setup() {
-  setCanvasContainer('canvas');
+let mover;
+let gravity;
+let mVec;
+let pMVec;
+let isDragging = false;
+let initialPos;
 
-  background(255);
-  noStroke();
-  fill('red');
+function setup() {
+  createCanvas(400, 400);
+  mover = new Mover(width / 2, height / 2, 40); // Increased the size to 40
+  gravity = createVector(0, 0.2);
+  mVec = createVector();
+  pMVec = createVector();
+  initialPos = createVector(mover.pos.x, mover.pos.y);
 }
 
 function draw() {
   background(255);
-  circle(mouseX, mouseY, 50);
+
+  if (!isDragging) {
+    const force = p5.Vector.mult(gravity, mover.mass);
+    mover.applyForce(force);
+    mover.update();
+    mover.edgeBounce();
+    mover.display();
+  } else {
+    // Calculate the direction from the current position to the mouse
+    const direction = createVector(mouseX - mover.pos.x, mouseY - mover.pos.y);
+
+    // Scale the direction vector to increase the force (acceleration)
+    const dragForce = direction.copy().normalize().mult(0.2); // You can adjust this value to control the acceleration
+
+    mover.applyForce(dragForce);
+    mover.update();
+    mover.display();
+  }
+}
+
+function mousePressed() {
+  if (mover.isHover) {
+    isDragging = true;
+    initialPos.set(mover.pos.x, mover.pos.y);
+  }
+}
+
+function mouseDragged() {
+  if (isDragging) {
+    mover.pos.set(mouseX, mouseY);
+  }
+}
+
+function mouseReleased() {
+  if (isDragging) {
+    isDragging = false;
+  }
 }
