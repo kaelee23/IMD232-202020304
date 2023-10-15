87812cdc90b0e641703f351a8e8fdb8a4e55c39b
kaelee23 <dahnlee@naver.com>
2023-10-15 22:57:51 +0900

    ㅓ

--- src/w4/sketches/sketch3forw4step2/script/sketch.js
+++ src/w4/sketches/sketch3forw4step2/script/sketch.js
@@ -7,7 +7,6 @@ function setup() {
   setCanvasContainer('canvas', 3, 2, true);
   background(255);
   moverA = new Mover(width / 3, height / 2, 10);
-  moverB = new Mover((2 * width) / 3, height / 2, 1);
   gravity = createVector(0, 0.1);
   wind = createVector(0.2, 0);
 }
@@ -31,21 +30,4 @@ function draw() {
   moverA.update();
   moverA.checkEdges();
   moverA.display();
-
-  let gravityB = createVector(gravity.x, gravity.y);
-  gravityB.mult(moverB.mass);
-  moverB.applyForce(gravityB);
-  if (mouseIsPressed && isMouseInsideCanvas()) {
-    moverB.applyForce(wind);
-  }
-  if (moverB.contactEdge()) {
-    let c = 0.9;
-    let friction = moverB.vel.copy();
-    friction.mult(-1);
-    friction.mult(c);
-    moverB.applyForce(friction);
-  }
-  moverB.update();
-  moverB.checkEdges();
-  moverB.display();
 }
