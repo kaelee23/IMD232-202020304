a2b6bb0e37fc9fc88feba953363250f5821c8741
kaelee23 <dahnlee@naver.com>
2023-10-15 21:45:19 +0900

    d

--- src/w5/assignment/step1/script/sketch.js
+++ src/w5/assignment/step1/script/sketch.js
@@ -1,33 +1,54 @@
-const cNum = 8;
 const rNum = 8;
-let gridC;
-let gridR;
-let angleBeginVel;
+const cNum = 8;
 let angleBegin = 0;
-let angleStep;
+let angleStep = 15;
 
 function setup() {
   setCanvasContainer('canvas', 1, 1, true);
-
-  colorMode(HSL, 360, 100, 100, 100);
   background(360, 0, 100);
+  colorMode(HSL, 360, 100, 100, 100);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(360, 0, 100);
+  let angle = angleBegin;
 
-  for (let r = 0; r < rNum; r++) {
-    for (let c = 0; c < cNum; c++) {
-      stroke((360 / rNum) * r, (360 / cNum) * c, 360);
-      let x = ((r + 1) * width) / (rNum + 1);
-      let y = ((c + 1) * hight) / (cNum + 1);
-      ellipse(x, y, 8);
+  for (let a = 0; a < rNum; a++) {
+    for (let b = 0; b < cNum; b++) {
+      //stroke((360 / rNum) * a, (100 / cNum) * b, 0);
+      stroke(0);
+      let x = (width / (cNum + 1)) * (b + 1);
+      let y = (height / (rNum + 1)) * (a + 1);
+
+      if (a % 2 === 0) {
+        if (b % 2 === 0) {
+          stroke(210, 100, 50);
+        } else {
+          stroke(100, 100, 40);
+        }
+      } else {
+        if (b % 2 === 0) {
+          stroke(0, 100, 80);
+        } else {
+          stroke(50, 100, 60);
+        }
+      }
+      ellipse(x, y, 40);
+
       push();
-      translate();
-      rotate();
+      translate(x, y);
+      rotate(angle);
+      line(0, 0, 20, 0);
+
+      noStroke(0);
+      fill(0);
+      ellipse(20, 0, 10);
       pop();
+
+      angle += angleStep;
     }
   }
 
-  angleBegin += angleBeginVel;
+  angleBegin += angleStep;
 }
