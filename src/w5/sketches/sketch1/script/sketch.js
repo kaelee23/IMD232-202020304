c2ae4b4d054bb8fb125c2c931714a6cc421f3129
kaelee23 <dahnlee@naver.com>
2023-10-15 21:41:13 +0900

    ㅇ

--- src/w5/sketches/sketch1/script/sketch.js
+++ src/w5/sketches/sketch1/script/sketch.js
@@ -1,23 +1,37 @@
 const rNum = 8;
 const cNum = 8;
-const rBegin = 0;
-const rGap = 0;
+let gridC;
+let gridR;
+let angleBeginVel;
+let angleBegin = 0;
+let angleStep;
 
 function setup() {
-  setCanvasContainer('canvas', 3, 2, true);
-
-  background(255);
+  c;
+  bcolorMode(HSL, 360, 100, 100, 100);
+  background(360, 0, 100);
 }
+
 function draw() {
   background(255);
 
-  rectMode(CENTER);
-  for (let a = rBegin; a < rNum; a++) {
-    for (let b = rGap; b < cNum; b++) {
-      stroke((255 / rNum) * a, (255 / cNum) * b, 255);
+  for (let a = 0; a < rNum; a++) {
+    for (let b = 0; b < cNum; b++) {
+      stroke((360 / rNum) * a, (360 / cNum) * b, 360);
+
       let x = ((a + 1) * width) / (rNum + 1);
       let y = ((b + 1) * height) / (cNum + 1);
+
       ellipse(x, y, 30);
+
+      //선
+      rotate((TAU / 360) * 15);
+      line(x, y, 30);
+      circle();
+      push();
+      translate();
+      rotate();
+      pop();
     }
   }
 }
