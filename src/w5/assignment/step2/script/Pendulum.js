8555ae33cdc238a127e9bc7f9228d5b616ada797
kaelee23 <dahnlee@naver.com>
2023-10-16 13:58:17 +0900

    s

--- src/w5/assignment/step2/script/Pendulum.js
+++ src/w5/assignment/step2/script/Pendulum.js
@@ -1,34 +1,96 @@
-let pendulumA;
-let gravity;
+class Pendulum {
+  constructor(x, y, length, angle, rad) {
+    this.angle = angle;
+    this.angleVel = 0;
+    this.angleAcc = 0;
+    this.pos = createVector(x, y);
+    this.length = length;
+    this.ballPos = createVector(x, y);
+    this.ballPos.add(
+      cos(this.angle) * this.length,
+      sin(this.angle) * this.length
+    );
+    this.rad = rad;
+    this.draggingOffset = createVector();
+    this.isHover = false;
+    this.isDragging = false;
+  }
 
-function setup() {
-  setCanvasContainer('canvas', 2, 1, true);
+  applyGravity(gravity) {
+    this.angleAcc =
+      (sin(this.angle - (TAU / 360) * 90) * -gravity.y) / this.length;
+  }
 
-  pendulumA = new Pendulum(width / 2, 10, height / 3, (TAU / 360) * 45, 25);
-  gravity = createVector(0, 0.5);
+  update() {
+    if (!this.isDragging) {
+      this.angleVel += this.angleAcc;
+      this.angle += this.angleVel;
+      this.angleVel *= 0.998;
+    }
+    this.ballPos.set(
+      cos(this.angle) * this.length + this.pos.x,
+      sin(this.angle) * this.length + this.pos.y
+    );
+  }
 
-  background(255);
-}
+  display() {
+    noStroke();
+    fill(127);
+    ellipse(this.pos.x, this.pos.y, 20);
+    if (this.isDragging) {
+      fill('#ff0000');
+    } else if (this.isHover) {
+      fill(127);
+    } else {
+      fill(191);
+    }
+    ellipse(this.ballPos.x, this.ballPos.y, 2 * this.rad);
+    stroke(0);
+    noFill();
+    line(this.pos.x, this.pos.y, this.ballPos.x, this.ballPos.y);
+  }
 
-function draw() {
-  pendulumA.applyGravity(gravity);
-  pendulumA.update();
-  background(255);
-  pendulumA.display();
-}
+  mouseMoved(mX, mY) {
+    this.isHover =
+      (this.ballPos.x - mX) ** 2 + (this.ballPos.y - mY) ** 2 <= this.rad ** 2;
+  }
 
-function mouseMoved() {
-  pendulumA.mouseMoved(mouseX, mouseY);
-}
+  mousePressed(mX, mY) {
+    if (this.isHover) {
+      this.isDragging = true;
+      this.angleAcc = 0;
+      this.angleVel = 0;
+      this.draggingOffset.set(mX - this.ballPos.x, mY - this.ballPos.y);
+    }
+  }
 
-function mousePressed() {
-  pendulumA.mousePressed(mouseX, mouseY);
-}
+  mouseDragged(mX, mY) {
+    if (this.isDragging) {
+      const ballShouldBe = createVector(
+        mX - this.draggingOffset.x,
+        mY - this.draggingOffset.y
+      );
+      const angle = atan2(
+        ballShouldBe.y - this.pos.y,
+        ballShouldBe.x - this.pos.x
+      );
+      this.angle = angle;
+    }
+  }
 
-function mouseDragged() {
-  pendulumA.mouseDragged(mouseX, mouseY);
-}
+  mouseReleased() {
+    this.isDragging = false;
+  }
 
-function mouseReleased() {
-  pendulumA.mouseReleased();
+  //connect(pendulumB) {
+  //  this.pendulumA.ballPos = pendulumB;
+  //}
+  //connect(pendulumB) {
+  //  this.pendulumA = pendulumB;
+  //}
+  connect(pendulumB) {
+    this.pendulumA = pendulumB;
+    // Update the position of pendulum B's ballPos to the center of pendulum A
+    pendulumB.ballPos.set(this.pos.x, this.pos.y);
+  }
 }
