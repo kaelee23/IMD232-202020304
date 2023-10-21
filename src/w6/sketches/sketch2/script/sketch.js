858f309d586ece705db8deeb952352763ab6bb0b
kaelee23 <dahnlee@naver.com>
2023-10-21 15:24:14 +0900

    hh

--- src/w6/sketches/sketch2/script/sketch.js
+++ src/w6/sketches/sketch2/script/sketch.js
@@ -1,12 +1,30 @@
-function setup() {
-  setCanvasContainer('canvas', 960, 640);
+let emitter;
+let particle;
+let g;
 
-  background(255);
-  noStroke();
-  fill('red');
+function setup() {
+  setCanvasContainer('canvas', 2, 1, true);
+
+  colorMode(HSL, 360, 100, 100);
+  particle = new Ball(width / 2, 0, 0, 0, 1, 0, 100, 50);
+
+  emitter = new Emitter(width / 2, height);
+
+  g = createVector(0, 0.1);
+
+  background('white');
 }
 
 function draw() {
-  background(255);
-  circle(mouseX, mouseY, 50);
+  background('white');
+  const scaledG = p5.Vector.mult(g, particle.mass);
+  particle.applyForce(scaledG);
+  particle.update();
+  particle.display();
+
+  emitter.createBall();
+  emitter.applyGravity(g);
+  emitter.update();
+  emitter.display();
+  console.log(emitter.balls.length);
 }
