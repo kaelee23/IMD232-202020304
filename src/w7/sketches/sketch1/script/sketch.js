970d0a37661db0b6e71127b70b5e25502cfdf8e6
kaelee23 <dahnlee@naver.com>
2023-10-23 14:09:54 +0900

    ㅇㅇ

--- src/w7/sketches/sketch1/script/sketch.js
+++ src/w7/sketches/sketch1/script/sketch.js
@@ -2,11 +2,8 @@ function setup() {
   setCanvasContainer('canvas', 3, 2, true);
 
   background(255);
-  noStroke();
-  fill('red');
 }
 
 function draw() {
   background(255);
-  circle(mouseX, mouseY, 50);
 }
