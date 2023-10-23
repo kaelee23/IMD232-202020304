966a62d244e530f64ce073fd54689748f823be25
kaelee23 <dahnlee@naver.com>
2023-10-23 14:31:18 +0900

    cc

--- src/w7/sketches/sketch1/script/vehicle.js
+++ src/w7/sketches/sketch1/script/vehicle.js
@@ -1,7 +1,9 @@
 class Vehicle {
-  constructor() {
-    this.pos;
-    this.vel;
-    this.acc;
+  constructor(x, y, rad) {
+    this.pos = createVector();
+    this.vel = createVector();
+    this.acc = createVector();
+    this.mass = 1;
+    this.rad = rad;
   }
 }
