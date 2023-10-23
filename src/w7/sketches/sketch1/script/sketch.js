9fd665aa466002cc34a2b649f5b5641fe2313bf0
kaelee23 <dahnlee@naver.com>
2023-10-23 16:25:30 +0900

    ㅊㅊ

--- src/w7/sketches/sketch1/script/sketch.js
+++ src/w7/sketches/sketch1/script/sketch.js
@@ -1,16 +1,21 @@
 let vehicle;
+let mVec;
+let debug = true;
+
 function setup() {
   setCanvasContainer('canvas', 2, 1, true);
 
   colorMode(HSL, 360, 100, 100, 100);
-  vehicle = new Vehicle(width / 2, height / 2, 16, color(330, 100, 50));
-
+  vehicle = new Vehicle(width / 2, height / 2, 16, 5, 0.5, color(330, 100, 50));
+  mVec = createVector();
   colorMode(RGB, 255, 255, 255);
   background(255);
 }
 
 function draw() {
-  vehicle.update();
   background(255);
+  mVec.set(mouse.x, mouse.y);
+  vehicle.seek(mVec);
+  vehicle.update();
   vehicle.display();
 }
--- src/w7/sketches/sketch2/script/sketch.js
+++ src/w7/sketches/sketch2/script/sketch.js
@@ -1,12 +1,22 @@
-function setup() {
-  setCanvasContainer('canvas', 960, 640);
+let Strach;
 
-  background(255);
-  noStroke();
-  fill('red');
+let debug = true;
+
+function setup() {
+  setCanvasContainer('canvas', 2, 1, true);
+
+  colorMode(HSL, 360, 100, 100, 100);
+  Strach = new Strach();
+
+  //colorMode(RGB, 255, 255, 255);
+  background(0, 100, 100);
 }
 
 function draw() {
   background(255);
-  circle(mouseX, mouseY, 50);
+  strach.run();
+}
+
+function mouseDragged() {
+  strach.addVehicle(mouseX, mouseY);
 }
