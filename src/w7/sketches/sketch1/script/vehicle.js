5080ab4d2a06d720936b84ed43f4a807ca189ce0
kaelee23 <dahnlee@naver.com>
2023-10-23 14:49:17 +0900

    ss

--- src/w7/sketches/sketch1/script/vehicle.js
+++ src/w7/sketches/sketch1/script/vehicle.js
@@ -5,5 +5,30 @@ class Vehicle {
     this.acc = createVector();
     this.mass = 1;
     this.rad = rad;
+    this.color = color;
+  }
+
+  applyForce(force) {
+    force.div(this.mass);
+    let calcedAcc = p5.Vector.div(force, this.mass);
+    this.acc.add(force);
+    this.acc.mult(0);
+  }
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+  }
+  display() {
+    push();
+    translate(this.pos.x, this.pos.y);
+    noStroke();
+    fill(this.color);
+    beginshape();
+    vertex(this.rad, 0);
+    vertex(this.rad * cos(radians(-135)), this.rad * sin(radians(-135)));
+    vertex(0, 0);
+    vertex(this.rad * cos(radians(135)), this.rad * sin(radians(135)));
+    endShape(CLOSE);
+    pop();
   }
 }
