f80733c5ca4b7694d2a1bedb5377ca4b267e236a
kaelee23 <dahnlee@naver.com>
2023-10-23 14:54:36 +0900

    xx

--- src/w7/sketches/sketch1/script/sketch.js
+++ src/w7/sketches/sketch1/script/sketch.js
@@ -1,9 +1,16 @@
+let vehicle;
 function setup() {
   setCanvasContainer('canvas', 2, 1, true);
 
+  colorMode(HSL, 360, 100, 100, 100);
+  vehicle = new Vehicle(width / 2, height / 2, 16, color(330, 100, 50));
+
+  colorMode(RGB, 255, 255, 255);
   background(255);
 }
 
 function draw() {
+  vehicle.update();
   background(255);
+  vehicle.display();
 }
