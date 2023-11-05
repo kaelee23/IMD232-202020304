ec2a6e69f2485f689b44efad307cc2ef21cb7704
kaelee23 <dahnlee@naver.com>
2023-11-05 22:54:07 +0900

    ㄹㄹ

--- src/w8/sketches/sketch1/script/sketch.js
+++ src/w8/sketches/sketch1/script/sketch.js
@@ -2,11 +2,17 @@ function setup() {
   setCanvasContainer('canvas', 3, 2, true);
 
   background(255);
-  noStroke();
-  fill('red');
 }
 
 function draw() {
   background(255);
-  circle(mouseX, mouseY, 50);
+  const text = '^(&*@#&#@($&*^*&^%#';
+  let result = '';
+
+  for (let i = 0; i < text.length; i++) {
+    result += ' '.repeat(i); // 현재 문자의 위치만큼 공백을 추가
+    result += text[i]; // 현재 문자를 추가
+  }
+
+  console.log(result);
 }
