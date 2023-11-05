ba0111b9f64374b8271b964095b5f6d80364f65c
kaelee23 <dahnlee@naver.com>
2023-11-06 01:36:28 +0900

    ㅌㅌ

--- src/w10/sketches/sketch1/script/sketch.js
+++ src/w10/sketches/sketch1/script/sketch.js
@@ -1,12 +1,29 @@
 function setup() {
-  setCanvasContainer('canvas', 3, 2, true);
-
+  createCanvas(400, 400);
   background(255);
-  noStroke();
-  fill('red');
+  noFill();
+  stroke('red');
+  strokeWeight(2);
 }
 
 function draw() {
   background(255);
-  circle(mouseX, mouseY, 50);
+
+  let centerX = width / 2;
+  let centerY = height / 2;
+
+  // 여러 개의 곡선 그리기
+  for (let i = 0; i < 5; i++) {
+    let radius = 50 + i * 20;
+    let numPoints = 10; // 곡선 상의 점 개수
+    let angleIncrement = TWO_PI / numPoints;
+
+    beginShape();
+    for (let j = 0; j < numPoints; j++) {
+      let x = centerX + radius * cos(j * angleIncrement);
+      let y = centerY + radius * sin(j * angleIncrement);
+      vertex(x, y);
+    }
+    endShape();
+  }
 }
