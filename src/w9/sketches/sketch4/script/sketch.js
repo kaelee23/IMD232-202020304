98e69c46b5ca08c3e1a273e1df1ff55055a528bb
kaelee23 <dahnlee@naver.com>
2023-11-11 22:00:06 +0900

    ㅇㅇ

--- src/w9/sketches/sketch4/script/sketch.js
+++ src/w9/sketches/sketch4/script/sketch.js
@@ -1,65 +1,160 @@
+// create variables
 let ropes = [];
 
 function setup() {
   createCanvas(800, 600);
 
-  // Add ropes
-  ropes.push(new Rope(100, 50, 8, 50, 20, color(255, 165, 0))); // Orange
-  ropes.push(new Rope(350, 50, 10, 20, 20, color(0, 0, 255))); // Blue
-  ropes.push(new Rope(600, 50, 13, 50, 20, color(0, 128, 0))); // Green
+  // add ropes
+  ropes.push(createRope(100, 50, 8, 50, 20, 25, 0.5, 0, -25, 0.8));
+  ropes.push(createRope(350, 50, 10, 20, 20, 20, 0.5, 0, -20, 0.8));
+  ropes.push(createRope(600, 50, 13, 50, 20, 5, 0.3, 0, -20, 1));
+
+  // add ground
+  createGround(400, 600, 1200, 50.5);
+
+  // add mouse control
+  let options = {
+    stiffness: 0.2,
+    render: { visible: false },
+  };
+  let mouseConstraint = Matter.Constraint.create({
+    pointA: { x: mouseX, y: mouseY },
+    bodyB: ropes[0][0],
+    pointB: { x: -25, y: 0 },
+    stiffness: 0.5,
+  });
+
+  World.add(world, mouseConstraint);
+
+  // fit the render viewport to the scene
+  let minBounds = createVector(0, 0);
+  let maxBounds = createVector(700, 600);
+  setBounds(minBounds, maxBounds);
 }
 
 function draw() {
   background(255);
 
-  // Update and display ropes
-  for (let rope of ropes) {
-    rope.update();
-    rope.display();
+  // Update physics engine
+  updatePhysics();
+
+  // Draw ropes
+  for (let i = 0; i < ropes.length; i++) {
+    drawRope(ropes[i]);
   }
 }
 
-class Rope {
-  constructor(x, y, segments, segmentWidth, segmentHeight, ropeColor) {
-    this.segments = [];
-    this.ropeColor = ropeColor;
+function createRope(
+  x,
+  y,
+  segments,
+  width,
+  height,
+  chamfer,
+  chainX,
+  chainY,
+  chainStiffness,
+  chainLength
+) {
+  let rope = [];
 
-    // Create segments
-    for (let i = 0; i < segments; i++) {
-      let segment = createSprite(
-        x + i * segmentWidth,
-        y,
-        segmentWidth,
-        segmentHeight
-      );
-      segment.shapeColor = ropeColor;
-      this.segments.push(segment);
+  for (let i = 0; i < segments; i++) {
+    let xPos = x + i * (width + 10);
+    let body;
+
+    if (chamfer > 0) {
+      body = createRoundedRectangle(xPos, y, width, height, chamfer);
+    } else {
+      body = createRectangle(xPos, y, width, height);
     }
 
-    // Connect segments with constraints
-    for (let i = 1; i < this.segments.length; i++) {
-      let constraint = createConstraint(this.segments[i - 1], this.segments[i]);
-      this.segments.push(constraint);
-    }
+    rope.push(body);
   }
 
-  update() {
-    // Apply forces or update properties if needed
+  for (let i = 0; i < segments - 1; i++) {
+    let constraint = Constraint.create({
+      bodyA: rope[i],
+      pointA: { x: chainX, y: chainY },
+      bodyB: rope[i + 1],
+      pointB: { x: -width / 2, y: 0 },
+      stiffness: chainStiffness,
+      length: chainLength,
+    });
+    World.add(world, constraint);
   }
 
-  display() {
-    drawSprites();
+  return rope;
+}
+
+function createRectangle(x, y, width, height) {
+  let options = {
+    friction: 0.8,
+    restitution: 0.3,
+  };
+  let body = Bodies.rectangle(x, y, width, height, options);
+  World.add(world, body);
+  return body;
+}
+
+function createRoundedRectangle(x, y, width, height, chamfer) {
+  let options = {
+    friction: 0.8,
+    restitution: 0.3,
+    chamfer: { radius: chamfer },
+  };
+  let body = Bodies.rectangle(x, y, width, height, options);
+  World.add(world, body);
+  return body;
+}
+
+function createGround(x, y, width, height) {
+  let options = {
+    isStatic: true,
+  };
+  let ground = Bodies.rectangle(x, y, width, height, options);
+  World.add(world, ground);
+}
+
+function drawRope(rope) {
+  for (let i = 0; i < rope.length; i++) {
+    drawBody(rope[i]);
   }
 }
 
-function createConstraint(bodyA, bodyB) {
-  let constraint = createSprite(
-    (bodyA.position.x + bodyB.position.x) / 2,
-    (bodyA.position.y + bodyB.position.y) / 2
-  );
-  constraint.visible = false;
-  constraint.immovable = true;
-  constraint.displace(bodyA);
-  constraint.displace(bodyB);
-  return constraint;
+function drawBody(body) {
+  let pos = body.position;
+  let angle = body.angle;
+
+  push();
+  translate(pos.x, pos.y);
+  rotate(angle);
+  rectMode(CENTER);
+  fill(127);
+  stroke(0);
+  strokeWeight(2);
+  if (body.vertices) {
+    beginShape();
+    for (let i = 0; i < body.vertices.length; i++) {
+      let vertexPos = body.vertices[i];
+      vertex(vertexPos.x, vertexPos.y);
+    }
+    endShape(CLOSE);
+  } else {
+    rect(0, 0, body.width, body.height);
+  }
+  pop();
+}
+
+function updatePhysics() {
+  Engine.update(engine);
+}
+
+function setBounds(min, max) {
+  let renderOptions = {
+    bounds: {
+      min: min,
+      max: max,
+    },
+  };
+  render.options(renderOptions);
 }
