e3316e5115c2facee261cbff7eae17b8bdad4168
kaelee23 <dahnlee@naver.com>
2023-11-12 19:04:30 +0900

    ㄴㄴ

--- src/w9/assignment/step1/script/sketch1.js
+++ src/w9/assignment/step1/script/sketch1.js
@@ -1,4 +1,30 @@
-// Initialize variables
+// Matter.js 라이브러리 컴포넌트들
+//let Engine = Matter.Engine,
+//Render = Matter.Render,
+//Runner = Matter.Runner,
+//Body = Matter.Body,
+//Composite = Matter.Composite,
+//Composites = Matter.Composites,
+//Constraint = Matter.Constraint,
+//MouseConstraint = Matter.MouseConstraint,
+//Mouse = Matter.Mouse,
+//Bodies = Matter.Bodies;
+
+let {
+  Engine,
+  Body,
+  Composite,
+  Composites,
+  Constraint,
+  MouseConstraint,
+  Mouse,
+  Bodies,
+} = Matter;
+
+// 엔진 생성
+let engine = Engine.create(),
+  world = engine.world;
+
 let groupA;
 let ropeA;
 
@@ -10,12 +36,8 @@ let ropeC;
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
-  background(255);
-}
-
-function draw() {
-  background(255);
-
+  // 바디 추가
+  // 주황색 도형 그룹
   groupA = Body.nextGroup(true);
 
   ropeA = Composites.stack(100, 50, 8, 1, 10, 10, function (x, y) {
@@ -126,4 +148,8 @@ function draw() {
   //console.log(ground);
 
   Runner.run(runner, engine);
+}
+function draw() {
+  //Engine.update(engine);
+  background(255);
 }
