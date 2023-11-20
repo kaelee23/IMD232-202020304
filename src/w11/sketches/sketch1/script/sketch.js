e00b46bb65762fcd8475be643a9d21f18b227d17
kaelee23 <dahnlee@naver.com>
2023-11-20 14:49:07 +0900

    ss

--- src/w11/sketches/sketch1/script/sketch.js
+++ src/w11/sketches/sketch1/script/sketch.js
@@ -5,9 +5,13 @@ function setup() {
   cam = createCapture(VIDEO);
   cam.size(1080, 1024);
   cam.hide();
+  //noLoop();
 }
 
 function draw() {
   background(255);
   image(cam, 0, 0, width, height);
+  cam.loadPixels(cam);
+  console.log('width', cam.width);
+  console.log('height', cam.height);
 }
