5a8e21437e0819a07da5ef11b26a31e5d0fcf599
kaelee23 <dahnlee@naver.com>
2023-12-14 13:18:50 +0900

    dd

---
+++ src/w12/final/finalSnow/script/sketch.js
@@ -0,0 +1,39 @@
+let particles = [];
+
+function setup() {
+  setCanvasContainer('canvas', 2, 1, true);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(255);
+  circle(mouseX, mouseY, 50);
+
+  if (random(1) < 1) {
+    let x = random(width);
+    let y = -10;
+    let rotationSpeed = random(-0.2, 0.3);
+    particles.push(new Particle(x, y, rotationSpeed));
+  }
+
+  for (let i = particles.length - 1; i >= 0; i--) {
+    let particle = particles[i];
+    particle.run();
+
+    if (particle.isDead()) {
+      particles.splice(i, 1);
+    }
+
+    // Check if the mouse is near the particle and apply a parabolic force
+    let mouseDist = dist(mouseX, mouseY, particle.pos.x, particle.pos.y);
+    if (mouseDist < 50) {
+      let force = createVector(
+        mouseX - particle.pos.x,
+        mouseY - particle.pos.y
+      );
+      force.normalize();
+      force.mult(0.1); // Adjust the strength of the force as needed
+      particle.applyForce(force);
+    }
+  }
+}
--- src/w13/sketches/sketch1/script/sketch.js
+++ src/w13/sketches/sketch1/script/sketch.js
@@ -1,6 +1,9 @@
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
 
+  // Set canvas size
+  createCanvas(600, 400); // Adjust the size as needed
+
   background(255);
   noStroke();
   fill('red');
@@ -9,4 +12,10 @@ function setup() {
 function draw() {
   background(255);
   circle(mouseX, mouseY, 50);
+
+  // '바람'이란 글자 그리기
+  textSize(48); // Increase text size for better visibility
+  fill('black'); // Text color
+  textAlign(CENTER, CENTER); // Center the text
+  text('바람', width / 2, height / 2); // Display at the center of the canvas
 }
